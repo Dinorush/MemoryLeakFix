2e052ab9061d2f230a63f6ba0d3c27131729a477
Dinorush <onparnorthstar@gmail.com>
2025-02-13 20:05:02 -0500

    Fix nullref + missing shooter bug colliders

--- MemoryLeakFix/Patches/FallingPatches.cs
+++ MemoryLeakFix/Patches/FallingPatches.cs
@@ -7,6 +7,27 @@ namespace MemoryLeakFix.Patches
     [HarmonyPatch]
     internal static class FallingPatches
     {
+        [HarmonyPatch(typeof(GlueClusterGrenadeInstance), nameof(GlueClusterGrenadeInstance.Start))]
+        [HarmonyPostfix]
+        private static void GlueGrenadeSpawn(GlueClusterGrenadeInstance __instance)
+        {
+            FallingObjectHandler.AddObject(__instance.gameObject);
+        }
+
+        [HarmonyPatch(typeof(GlowstickInstance), nameof(GlowstickInstance.Setup))]
+        [HarmonyPostfix]
+        private static void GlowstickSpawn(GlowstickInstance __instance)
+        {
+            FallingObjectHandler.AddObject(__instance.gameObject);
+        }
+
+        [HarmonyPatch(typeof(FogRepellerInstance), nameof(FogRepellerInstance.Start))]
+        [HarmonyPostfix]
+        private static void FogRepellerSpawn(FogRepellerInstance __instance)
+        {
+            FallingObjectHandler.AddObject(__instance.gameObject);
+        }
+
         [HarmonyPatch(typeof(GlueGunProjectile), nameof(GlueGunProjectile.Awake))]
         [HarmonyPostfix]
         private static void GlueGunSpawn(GlueGunProjectile __instance)
@@ -19,6 +40,8 @@ namespace MemoryLeakFix.Patches
         private static void DropMag(BulletWeapon __instance)
         {
             var pool = __instance.m_magDropPool;
+            if (pool == null) return;
+
             var go = pool.m_freeInstances.Count > 0 ? pool.m_freeInstances.First.Value : pool.m_usedInstances.First.Value;
             FallingObjectHandler.AddObject(go, pool.Return);
         }
