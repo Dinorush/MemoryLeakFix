47e1e24ab8872f5972b139a5fd99be4fc2b9a0e3
Dinorush <onparnorthstar@gmail.com>
2025-03-04 17:59:13 -0500

    Version bump

--- MemoryLeakFix/EntryPoint.cs
+++ MemoryLeakFix/EntryPoint.cs
@@ -5,7 +5,7 @@ using MemoryLeakFix.Handler;
 
 namespace MemoryLeakFix
 {
-    [BepInPlugin("Dinorush." + MODNAME, MODNAME, "1.2.1")]
+    [BepInPlugin("Dinorush." + MODNAME, MODNAME, "1.2.2")]
     [BepInDependency("dev.gtfomodding.gtfo-api", BepInDependency.DependencyFlags.HardDependency)]
     internal sealed class EntryPoint : BasePlugin
     {
