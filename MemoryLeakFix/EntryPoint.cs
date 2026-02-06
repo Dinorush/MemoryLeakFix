a2025e370451d6f43d9f610a47c167c30569899b
Dinorush <onparnorthstar@gmail.com>
2026-02-05 20:52:42 -0500

    Fix melee packet duplication

--- MemoryLeakFix/EntryPoint.cs
+++ MemoryLeakFix/EntryPoint.cs
@@ -5,7 +5,7 @@ using MemoryLeakFix.Handler;
 
 namespace MemoryLeakFix
 {
-    [BepInPlugin("Dinorush." + MODNAME, MODNAME, "1.3.3")]
+    [BepInPlugin("Dinorush." + MODNAME, MODNAME, "1.3.4")]
     [BepInDependency("dev.gtfomodding.gtfo-api", BepInDependency.DependencyFlags.HardDependency)]
     internal sealed class EntryPoint : BasePlugin
     {
---
+++ MemoryLeakFix/Patches/MeleePatches.cs
@@ -0,0 +1,44 @@
+﻿using Expedition;
+using GameData;
+using Gear;
+using HarmonyLib;
+using UnityEngine;
+
+namespace MemoryLeakFix.Patches
+{
+    [HarmonyPatch]
+    internal static class MeleePatches
+    {
+        private static SNetwork.SNet_BroadcastAction<pMeleeHitPacketStruct>? s_meleePacket;
+        [HarmonyPatch(typeof(MeleeWeaponFirstPerson), nameof(MeleeWeaponFirstPerson.Setup))]
+        [HarmonyPrefix]
+        private static bool FixPacket(MeleeWeaponFirstPerson __instance, ItemDataBlock data)
+        {
+            if (s_meleePacket != null)
+            {
+                __instance.ItemDataBlock = data;
+                __instance.PublicName = data.publicName;
+                __instance.m_expeditionGearComponent = __instance.GetComponent<ExpeditionGear>();
+                if (__instance.m_expeditionGearComponent != null)
+                    __instance.m_expeditionGearComponent.Setup();
+                __instance.Sound = new CellSoundPlayer();
+                __instance.SightLookAlign = __instance.transform.FindChildRecursive(__instance.ItemDataBlock.SightLookAlign);
+                __instance.MuzzleAlign = __instance.transform.FindChildRecursive(__instance.ItemDataBlock.MuzzleAlign);
+                __instance.BackpackAlign = __instance.transform.FindChildRecursive(__instance.ItemDataBlock.BackpackAlign);
+                __instance.enabled = false;
+                __instance.ModelData = __instance.GetComponentInChildren<MeleeWeaponModelData>();
+                __instance.WeaponAnimator = __instance.ModelData.m_animator;
+                __instance.m_hitPacket = s_meleePacket;
+                return false;
+            }
+            return true;
+        }
+
+        [HarmonyPatch(typeof(MeleeWeaponFirstPerson), nameof(MeleeWeaponFirstPerson.Setup))]
+        [HarmonyPostfix]
+        private static void CachePacket(MeleeWeaponFirstPerson __instance)
+        {
+            s_meleePacket = __instance.m_hitPacket;
+        }
+    }
+}
