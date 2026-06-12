55c1e8772c8d49535291da5d4c649c152758b866
Dinorush <onparnorthstar@gmail.com>
2026-06-12 03:12:38 -0400

    Fix loadout menu mem leak

---
+++ MemoryLeakFix/Patches/CellMenuPatches.cs
@@ -0,0 +1,16 @@
+﻿using CellMenu;
+using HarmonyLib;
+
+namespace MemoryLeakFix.Patches
+{
+    internal static class CellMenuPatches
+    {
+        [HarmonyPatch(typeof(CM_ScrollWindow), nameof(CM_ScrollWindow.ResetHeaders))]
+        [HarmonyPostfix]
+        private static void Post_ResetHeaders(CM_ScrollWindow __instance)
+        {
+            // Never cleared, thus adds duplicates every time the window is opened
+            __instance.m_nonContentItems?.Clear();
+        }
+    }
+}
